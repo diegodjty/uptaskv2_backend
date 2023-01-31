ea14725340bbfd66890b77211d5adb3bf05552b2
Diego Taveras <diegotaveras2@outlook.com>
2023-01-31 14:44:06 -0500

    allow cors with whitelist

--- index.js
+++ index.js
@@ -12,22 +12,21 @@ app.use(express.json());
 dotenv.config();
 connectDB();
 
-// Setup CORS
-// const whiteList = [process.env.CLIENT_URL];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whiteList.indexOf(origin) !== -1) {
-//       // Can accesss the api
-//       callback(null, true);
-//     } else {
-//       // Cannot access the api
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// };
+const whiteList = [process.env.CLIENT_URL];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1) {
+      // Can accesss the api
+      callback(null, true);
+    } else {
+      // Cannot access the api
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
+
 app.use(
   bodyParser.urlencoded({
     extended: true,
